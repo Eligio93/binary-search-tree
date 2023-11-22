0926e29479583cf18b89bcde81d3d60ba0d67a26
Eligio93 <eligio.cristantielli@gmail.com>
2023-11-22 23:23:06 +1100

    builds the tree from the given array and print it

--- index.js
+++ index.js
@@ -5,3 +5,36 @@ class Node{
         this.left=left
     }
 }
+class Tree{
+   
+    constructor(array){
+        this.root= this.buildTree(array,0,(array.length)-1);
+    }
+    buildTree(array,start,end){
+        if(start>end){
+            return null
+        }else{
+            let half=Math.round((start+end)/2);        
+            this.root=new Node(array[half],this.buildTree(array,start,half-1),this.buildTree(array,half+1,end));    
+        }
+        return this.root
+        
+    
+    }
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+let array=[1,2,3,4,5]
+let tree=new Tree(array)
+prettyPrint(tree.root)
